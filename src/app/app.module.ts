4af2fb1be1caa1ace915ef3dd6d1dee1d3ab92d7
thepowderriverfarrier <thepowderriverfrrier@Daniels-MBP.fios-router.home>
2017-09-27 11:25:58 -0400

    scanner
    Ionic barcode scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,52 @@
+//Daniels-MBP:scanner thepowderriverfrrier$ ionic cordova plugin add phonegap-plugin-barcodescanner
+//> cordova plugin add phonegap-plugin-barcodescanner --save
+//✔ Running command - done!
+//Fetching plugin "phonegap-plugin-barcodescanner" via npm
+//Saved plugin info for "phonegap-plugin-barcodescanner" to config.xml
+//Daniels-MBP:scanner thepowderriverfrrier$ npm install --save @ionic-native/barcode-scanner
+//+ @ionic-native/barcode-scanner@4.2.1
+
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
+  //components directives and pipes
   declarations: [
     MyApp,
     HomePage
   ],
+  //import modules
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
+  //components created at launch
   bootstrap: [IonicApp],
+  //any component loaded by type
+  //  compile a bundle for components actually used
   entryComponents: [
     MyApp,
     HomePage
   ],
+  //register services for dependency injection
+  //  if not here then in individual component's
+  //  @Component decorator
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BarcodeScanner
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner, BarcodeScannerOptions, BarcodeScanResult} from '@ionic-native/barcode-scanner';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  // type returned by scan function is BarcodeScanResult
+  result: BarcodeScanResult;
+  dataToEncode: string;
+
+  constructor(private barcode: BarcodeScanner, public navCtrl: NavController) {}
+
+  async encodeData(){
+    try{
+      await this.barcode.encode(this.barcode.Encode.TEXT_TYPE, this.dataToEncode);
+    }
+    catch(error){
+      console.error(error);
+    }
+  }
+
+
+  async scanBarcode(){
+    try {
+
+      const options: BarcodeScannerOptions = {
+        prompt: 'Align camera with barcode',
+        torchOn: true
+      }
+
+      this.result = await this.barcode.scan(options);
+
+    }
+    catch(error){
+      console.error(error);
+    }
 
   }
 
